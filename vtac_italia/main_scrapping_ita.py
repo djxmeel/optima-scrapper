b7878c925879a19815a4f529fd088598b5acbbf3
Djamel (David workstation) <djamelnadour15@gmail.com>
2023-09-06 09:29:48 +0200

    Finished es & ita 1st scrape

--- vtac_italia/main_scrapping_ita.py
+++ vtac_italia/main_scrapping_ita.py
@@ -17,9 +17,9 @@ class ScraperVtacItalia:
     print(f'LOGGER CREATED: {logger_path}')
 
     # Datos productos
-    IF_EXTRACT_ITEM_INFO = True
+    IF_EXTRACT_ITEM_INFO = False
     # PDFs productos
-    IF_DL_ITEM_PDF = True
+    IF_DL_ITEM_PDF = False
     # Enlaces productos en la página de origen
     IF_EXTRACT_ITEM_LINKS = False
     # Todos los campos de los productos a implementar en ODOO
