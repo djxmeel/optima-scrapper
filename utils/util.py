b0869e52c3de99ebed56e5420ab910926e853e51
Djamel (David workstation) <djamelnadour15@gmail.com>
2023-11-03 13:47:41 +0100

    CHANGE: translator better exception handling

--- utils/util.py
+++ utils/util.py
@@ -103,7 +103,7 @@ class Util:
             print('TRANSLATION TIMED OUT. Retrying...')
             time.sleep(3)
             return Util.translate_from_to_spanish(_from, text)
-        except AttributeError:
+        except AttributeError and TypeError:
             print(f'{text} NOT TRANSLATABLE. SKIPPING...')
 
         return text
