060afa480a46f8c67e31532f8cf416c4a5aa03d6
Djamel (David workstation) <djamelnadour15@gmail.com>
2023-11-29 13:33:34 +0100

    UPDATED: pathing

--- main/main_imp.py
+++ main/main_imp.py
@@ -21,7 +21,7 @@ PUBLIC_CATEGORIES_FILE_PATH = 'data/common/excel/public_categories_odoo.xlsx'
 SUPPLIER_STOCK_EXCEL_FILE_PATH = 'data/common/excel/supplier_stock.xlsx'
 SUPPLIER_PRICELIST_EXCEL_FILE_PATH = 'data/common/excel/pricelist_compra_coste.xlsx'
 
-SKUS_CATALOGO_Q12024_FILE_PATH = 'data/common/excel/public_category_sku.xlsx'
+SKUS_CATALOGO_Q12024_FILE_PATH = 'data/common/excel/public_category_sku_Q1_2024.xlsx'
 
 IF_IMPORT_FIELDS = False
 
