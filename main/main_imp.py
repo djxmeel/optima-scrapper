787a6444a72200fce6780ad0e76f957a1fab4fc1
OniiYhivi <djamelnadour15@gmail.com>
2024-01-22 12:44:48 +0100

    UPDATED: todos

--- main/main_imp.py
+++ main/main_imp.py
@@ -19,7 +19,6 @@ from utils.util import Util
 
 # TODO reescrape and reupload attachments to spec sheets field
 # TODO new catalog icons excel
-# TODO ADD Alexa icon to icon folder with name alexa.png
 # TODO run a descatologados import after running a full scrape
 # TODO generate a json with icon img name as key and b64 as value to upload icons to Odoo
 # TODO RUN remove_hyperlinks_and_qr_code_from_pdfs() from playground.py
