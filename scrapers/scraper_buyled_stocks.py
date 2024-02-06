ba3bdde7ee68604d1f5a29e87c9beb66b7b3204d
OniiYhivi <djamelnadour15@gmail.com>
2024-02-06 18:05:38 +0100

    NEW: set up start_scrape logic

--- scrapers/scraper_buyled_stocks.py
+++ scrapers/scraper_buyled_stocks.py
@@ -1,5 +1,12 @@
+import time
+from datetime import datetime
+
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+
+import utils.playground as playground
+from utils.odoo_import import OdooImport
+from utils.util import Util
 
 # TODO - Implement the scraper for the BuyLed stocks
 # The scraper should be able to:
@@ -21,12 +28,30 @@ class ScraperBuyLedStocks():
     appium_server_url = 'http://localhost:4723'
     driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
 
-    @classmethod
-    def start_scrape(self, output_dir_path):
-        pass
+    edittext_field = '//android.widget.EditText[@text="Búsqueda SKU"]'
+    search_btn = '//android.widget.EditText[@text="Búsqueda SKU"]/android.widget.Button'
+    stock_ita_text = "(//android.view.View[@content-desc='0'])[2]"
+    stock_buyled_text = "(//android.view.View[@content-desc='0'])[1]"
 
     @classmethod
-    def get_stock_data(self, sku):
+    def start_scrape(cls, output_dir_path):
+        products_odoo = OdooImport.browse_all_products_in_batches()
+        stock_data = []
+
+        for product in products_odoo:
+            sku = product.default_code
+            cls.search_sku(sku)
+            time.sleep(1)
+            stock_data.append(cls.get_stock_data(sku))
+
+        Util.dump_to_json(stock_data, f'{output_dir_path}/buyled_stocks_{datetime.now().strftime("%m-%d-%Y, %Hh %Mmin %Ss")}.json')
+        cls.end_scrape()
+    @classmethod
+    def end_scrape(cls):
+        cls.driver.quit()
+
+    @classmethod
+    def get_stock_data(cls, sku):
         return {
             'sku': sku,
             'stock_buyled': 0,
@@ -34,5 +59,5 @@ class ScraperBuyLedStocks():
         }
 
     @classmethod
-    def search_sku(self, sku):
+    def search_sku(cls, sku):
         pass
